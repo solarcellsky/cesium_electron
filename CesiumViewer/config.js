481a2921b37091fdfd09e6fa79b7e4b30757ee91
solar <solarcellsky@gmail.com>
2021-11-24 16:21:29 +0800

    refactor: 💡 init code

---
+++ CesiumViewer/config.js
@@ -0,0 +1,13 @@
+export const $config = {
+  //Cesium API AccessToken
+  CESIUM_ACCESS_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3ZWFiNTkyNC05NDVmLTQ1NjktODIwMy1kZTE4ZTc4ZGE4ZjAiLCJpZCI6NTk3MTEsImlhdCI6MTYyNDQyNTM2MX0.9dgD9B04smS8WfvZwmIBx6aOrjFW2POsVmWq8J60Uo8",
+  TMS_IMAGERY_URL: "",
+  // 大兴机场地理坐标
+  defaultPosition: [116.410088824153, 39.509140438939355],
+  // 地图默认指向 视角
+  defaultOrientation: {
+    heading: 6.283185307179581,
+    pitch: -1.5688168484696687,
+    roll: 0.0
+  }
+}
